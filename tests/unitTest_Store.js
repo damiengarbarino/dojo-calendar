b9c0988759cda81ae4ac72c8b7cc3c40389824c1
Christophe Jolif <cjolif@gmail.com>
2012-01-23 15:02:53 +0100

    remove dead code

--- tests/unitTest_Store.js
+++ tests/unitTest_Store.js
@@ -3,7 +3,6 @@ define(["doh", "../Calendar", "dojo/_base/Deferred", "dojo/store/JsonRest"],
 	doh.register("tests.unitTest_Store", [
 		function test_Error(t){
 			var calendar = new Calendar();
-			var ok, notok;
 			var d = Deferred.when(calendar.set("store", new JsonRest({ target: "/" }), function(){
 				t.f(true, "ok fct must not have been called");
 			}, function(){
