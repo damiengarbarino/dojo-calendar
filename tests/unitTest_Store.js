191110f38ba07a733bb0e3bead985583a23f7733
Christophe Jolif <cjolif@gmail.com>
2012-01-23 14:57:03 +0100

    fix naming

--- tests/unitTest_Store.js
+++ tests/unitTest_Store.js
@@ -2,14 +2,14 @@ define(["doh", "../Calendar", "dojo/_base/Deferred", "dojo/store/JsonRest"],
 	function(doh, Calendar, Deferred, JsonRest){
 	doh.register("tests.unitTest_Store", [
 		function test_Error(t){
-			var treeMap = new Calendar();
+			var calendar = new Calendar();
 			var ok, notok;
-			var d = Deferred.when(treeMap.set("store", new JsonRest({ target: "/" }), function(){
+			var d = Deferred.when(calendar.set("store", new JsonRest({ target: "/" }), function(){
 				t.f(true, "ok fct must not have been called");
 			}, function(){
 				t.t(true, "failure fct must have been called");
 			}));
-			treeMap.startup();
+			calendar.startup();
 		}
 	]);
 });
