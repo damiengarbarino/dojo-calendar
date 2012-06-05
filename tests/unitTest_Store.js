d4975f88e3cd54ba081e5a21821db1c8208b574a
Damien Garbarino <damiengarbarinoibm@gmail.com>
2012-06-05 15:24:28 +0200

    Fixed store unit test

--- tests/unitTest_Store.js
+++ tests/unitTest_Store.js
@@ -3,7 +3,7 @@ define(["doh", "../Calendar", "dojo/when", "dojo/store/JsonRest"],
 	doh.register("tests.unitTest_Store", [
 		function test_Error(t){
 			var calendar = new Calendar();
-			var d = when.when(calendar.set("store", new JsonRest({ target: "/" }), function(){
+			var d = when(calendar.set("store", new JsonRest({ target: "/" }), function(){
 				t.f(true, "ok fct must not have been called");
 			}, function(){
 				t.t(true, "failure fct must have been called");
