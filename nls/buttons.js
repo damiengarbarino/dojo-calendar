0d92d3dd94ca27c77f5055202198ad6241e3a520
Christophe Jolif <cjolif@gmail.com>
2013-04-08 17:06:51 +0200

    adding bg, hr and uk locales

--- nls/buttons.js
+++ nls/buttons.js
@@ -12,6 +12,7 @@ define({ root: {
 ,
 "zh": true,
 "zh-tw": true,
+"uk": true,
 "tr": true,
 "th": true,
 "sv": true,
@@ -29,6 +30,7 @@ define({ root: {
 "ja": true,
 "it": true,
 "hu": true,
+"hr": true,
 "he": true,
 "fr": true,
 "fi": true,
@@ -38,5 +40,6 @@ define({ root: {
 "da": true,
 "cs": true,
 "ca": true,
+"bg": true,
 "ar": true
 });
