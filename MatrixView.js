fac56a51704d0fe83d9f9458db5ba7080e940ebc
Damien Garbarino <damiengarbarinoibm@gmail.com>
2013-10-17 14:00:58 +0200

    Fixed regression: if oldRenderData was invalid, reset columnCount/rowCount

--- MatrixView.js
+++ MatrixView.js
@@ -442,6 +442,17 @@ function(
 				return;
 			}
 			
+			if(oldRenderData){
+				// make sure to have correct rowCount and columnCount
+				if(this.itemContainerTable){
+					var rows = query(".dojoxCalendarItemContainerRow", this.itemContainerTable);
+					var cols = query(".dojoxCalendarContainerRow", rows[0]);
+					oldRenderData.columnCount = cols.length;
+					oldRenderData.rowCount = rows.length;										
+				}
+				
+			}
+			
 			this._buildColumnHeader(renderData, oldRenderData);
 			this._buildRowHeader(renderData, oldRenderData);
 			this._buildGrid(renderData, oldRenderData);
@@ -611,8 +622,8 @@ function(
 			}else{ 
 				tbody = domConstruct.create("tbody", null, rowHeaderTable);
 			}				
-						
-			var count = renderData.rowCount - query("tr", rowHeaderTable).length;
+			
+			var count = renderData.rowCount - (oldRenderData ? oldRenderData.rowCount : 0);
 			
 			// Build HTML structure
 			if(count>0){ // creation
@@ -718,7 +729,7 @@ function(
 			var rowDiff = renderData.rowCount - currentTR.length;
 			var addRows = rowDiff > 0;
 			
-			var colDiff  = renderData.columnCount - (currentTR ? query("td", currentTR[0]).length : 0);
+			var colDiff  = renderData.columnCount - (oldRenderData ? oldRenderData.columnCount : 0);
 			
 			if(has("ie") == 8){
 				// workaround Internet Explorer 8 bug.
@@ -894,7 +905,7 @@ function(
 			
 			var rows = [];
 	
-			var count = renderData.rowCount - (oldRenderData ? oldRenderData.rowCount : 0);
+			var count = renderData.rowCount - (oldRenderData ? oldRenderData.rowCount : 0)
 			
 			if(has("ie") == 8){
 				// workaround Internet Explorer 8 bug.
