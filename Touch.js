94aa7c59c01ecbf78499722919848b31f7f6f813
Damien Garbarino <damiengarbarinoibm@gmail.com>
2014-12-30 11:54:05 +0100

    Fixed #105 Added 10px tolerance in touch.js to select item

--- Touch.js
+++ Touch.js
@@ -170,10 +170,15 @@ define(["dojo/_base/array", "dojo/_base/lang", "dojo/_base/declare", "dojo/dom",
 				}			
 			}
 			
-			p.touchMoved = true;
-								
-			if(this._editingGesture){				
-			
+			// 10px tolerance to select the item (#105)
+			if(!p.touchMoved &&
+				(Math.abs(touch.x - p.start.x) > 10 ||
+				 Math.abs(touch.y - p.start.y) > 10)) {
+				p.touchMoved = true;
+			}
+
+			if(this._editingGesture){
+
 				event.stop(e);
 				
 				if(p.itemBeginDispatched){
